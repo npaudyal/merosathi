9ebc115e63891d1b42d5251e8f0f7a05262c466b
npaudyal <npaudyal@go.olemiss.edu>
2020-06-10 17:38:18 -0500

    Profile page added

--- lib/ui/widgets/profileForm.dart
+++ lib/ui/widgets/profileForm.dart
@@ -2,9 +2,13 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:merosathi/bloc/authentication/authentication_bloc.dart';
+import 'package:merosathi/bloc/authentication/authentication_event.dart';
 import 'package:merosathi/bloc/profile/bloc.dart';
 import 'package:merosathi/repositories/userRepository.dart';
+import 'package:merosathi/ui/constants.dart';
 
 class ProfileForm extends StatefulWidget {
 
@@ -46,17 +50,129 @@ class _ProfileFormState extends State<ProfileForm> {
 
   }
 
+  _onSubmitted() async {
+
+
+    await _getlocation();
+    _profileBloc.add(Submitted(
+      name: _nameController.text,
+      age: age,
+      location: location,
+      gender: gender,
+      interestedIn: interestedIn,
+      photo: photo
+
+    ));
+  }
   
+
 
 
   UserRepository get _userRepository =>widget._userRepository;
 
-  
 
+@override
+  void initState() {
+
+    _getlocation();
+    _profileBloc = BlocProvider.of<ProfileBloc>(context);
+    super.initState();
+  }
+  
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
+    Size size = MediaQuery.of(context).size;
+
+    return BlocListener<ProfileBloc, ProfileState>(
+      listener: (context, state) {
+        if(state.isFailure) {
+          Scaffold.of(context)..hideCurrentSnackBar()
+         ..showSnackBar(SnackBar(
+              content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Profile Creation Unsuccessful"),
+
+                Icon(Icons.error),
+              ],
+            )));
+        }
+        if(state.isSubmitting) {
+          Scaffold.of(context)..hideCurrentSnackBar()
+         ..showSnackBar(SnackBar(
+              content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Submitting"),
+                
+                CircularProgressIndicator(),
+              ],
+            )));
+        }
+        if(state.isSuccess) {
+          BlocProvider.of<AuthenticationBloc>(context).add(LoggedIn());
+        }
       
-    );
+      },
+
+      child: BlocBuilder<ProfileBloc, ProfileState>(
+        builder: (context, state) {
+          return SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              color: backgroundColor,
+              width: size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: size.width,
+                    child: CircleAvatar(radius:size.width * 0.3,
+                    backgroundColor: Colors.transparent,
+                    
+                    ),
+                    
+
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      
+      
+      );
+  }
+
+  Widget textFieldWidget(controller, text, size){
+    return Padding(
+      
+      padding: EdgeInsets.all(size.height*0.02),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: text,
+
+          labelStyle: TextStyle(color: Colors.white, fontSize: size.height*0.03),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white, width: 1.0),
+
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white, width: 1.0),
+
+          ),
+
+        ),
+
+      ),
+
+      );
   }
 }
